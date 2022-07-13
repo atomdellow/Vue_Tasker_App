7a990aa1704c3ed51ebdc3c7b59e85f3ad61f416
Adam Dellow <atomdellow@gmail.com>
2022-07-13 12:50:27 -0400

    initial commit, showing the completed app, without the persistence using JSON server

---
+++ src/App.vue
@@ -0,0 +1,123 @@
+/* eslint-disable */
+<template>
+  <div class="container">
+    <HeaderComp 
+      @toggle-add-task="toggleAddTask" 
+      title="Task Tracker" 
+      :showAddTask="showAddTask" 
+    />
+    <div v-show="showAddTask">
+      <AddTask 
+        @add-task="addTask"
+      />
+    </div>
+    <Tasks 
+      @toggle-reminder="toggleReminder" @delete-task="deleteTask" 
+      :tasks="tasks"
+    />
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/Header'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+    Tasks,
+    AddTask
+},
+data(){
+  return {
+    tasks: [],
+    showAddTask: false
+  }
+},
+methods: {
+  toggleAddTask(){
+    this.showAddTask = !this.showAddTask
+  },
+  addTask(task) {
+    this.tasks = [...this.tasks, task]
+  },
+  deleteTask(id){
+    if(confirm('Are you sure?')){
+    this.tasks = this.tasks.filter((task)=>task.id !== id)
+    }
+  },
+  toggleReminder(id){
+    // this is a flipflop where the ternary detects it's state and sets it to the opposite based on event.
+    this.tasks = this.tasks.map((task)=> task.id === id ? { ...task, reminder: !task.reminder } : task)
+  },
+},
+created() {
+  this.tasks = [
+    {
+      id: 1,
+      text: "Doctor's Appointment",
+      day: "March 1st at 2:30pm",
+      reminder: true,
+    },
+        {
+      id: 2,
+      text: "Meeting at School",
+      day: "March 7th at 9am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "March 2nd at 10:am",
+      reminder: false,
+    },
+  ]
+},
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+/* eslint-disable */
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <NewBtn 
+            @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close':'Add Task'" 
+            :color="showAddTask ? 'red' : 'green'" 
+        />
+    </header>
+ 
+</template>
+
+<script>
+import NewBtn from './NewBtn.vue';
+
+    export default{
+    name: "HeaderComp",
+    props: {
+        title: {
+            title: String,
+        showAddTask: Boolean
+        },
+        components: {
+            NewBtn,
+        },
+    },
+    components: { NewBtn },
+}
+</script>
+
+<style scoped>
+    Header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
